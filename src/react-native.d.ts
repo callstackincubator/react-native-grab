8a5f2e039af8c2aeb058206aff54d58f1bdbf39f
Szymon Chmal <szymon@chmal.it>
2026-03-06 11:39:38 +0100

    Remove unused react-native type override

--- src/react-native.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "react-native/Libraries/Core/Devtools/parseErrorStack" {
-  export type ReactNativeGrabStackFrame = {
-    column: number | null | undefined;
-    file: string | null | undefined;
-    lineNumber: number | null | undefined;
-    methodName: string;
-    collapse: boolean;
-  };
-}
